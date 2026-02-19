3c5bdd6b02fb66d97da8faf77ef22b776067ab45
emersonilan <emerson.ilan@aluno.senac.rio>
2026-02-19 20:30:05 -0300

    ok

---
+++ static/js/firebaseConfig.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCj1rPEQ3rv1f2C6IHwfhva-5QFCZnDuhs",
+  authDomain: "projeto-flask-crud-de4eb.firebaseapp.com",
+  projectId: "projeto-flask-crud-de4eb",
+  storageBucket: "projeto-flask-crud-de4eb.firebasestorage.app",
+  messagingSenderId: "82760529790",
+  appId: "1:82760529790:web:389dda69deec7d2dfe252f"
+};
