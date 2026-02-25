cd82a68308c022612cda9805b9a567edcca5e4b8
emersonilan <emerson.ilan@aluno.senac.rio>
2026-02-24 21:28:56 -0300

    blueprints

---
+++ config.py
@@ -0,0 +1,20 @@
+# config.py
+
+# Configurações do site / aplicativo
+APP = {
+    # Nome do site para a tag `<title>...</title>`
+    'title': 'MyPyPad',
+
+    # Nome / logo do site, em HTML, para a tag `.navbar-brand` e outros usos
+    'name': 'My<i class="bi bi-filetype-py text-warning px-0"></i>Pad',
+
+    # Chave secreta (48 caracteres)
+    # Obtenha essa chave rodando `python keygen.py`
+    'secret_key': '697ab25e2fe2dfd30ae3c9e0ba5a563d51d2a4733f3dc09e92b8f45144db89aa007d32b4d2fe0eed9027dcc3bbee7538',
+}
+
+# Configurações do banco de dados
+DB = {}
+
+# Configurações dos cookies
+COOKIE = {}
