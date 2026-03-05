bc7a6b3a2e98d5a3864a6812c8abd13feb752452
emersonilan <emerson.ilan@aluno.senac.rio>
2026-03-05 18:55:00 -0300

    ok

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 # app.py
 
 from flask import Flask
-
-# Importa a constante `APP` de `config.py`
 from config import APP
 
-# Importa as blueprints
 from pages.home import home_bp
-from pages.about import about_bp
+from pages.about import info_bp
 from pages.contacts import contacts_bp
+from pages.login import login_bp
+from pages.newpad import newpad_bp
+from pages.search import search_bp
 
 app = Flask(__name__)
 
-# Injeta as variáveis globalmente, com os valores de `config.APP`
+
 @app.context_processor
 def inject_globals():
     return {
@@ -20,10 +20,13 @@ def inject_globals():
         "app_name": APP["name"],
     }
 
-# Registra as blueprints
-app.register_blueprint(home_bp)
-app.register_blueprint(about_bp)
-app.register_blueprint(contacts.bp)
 
-if __name__ == '__main__':
+app.register_blueprint(home_bp)
+app.register_blueprint(info_bp)
+app.register_blueprint(contacts_bp)
+app.register_blueprint(login_bp)
+app.register_blueprint(newpad_bp)
+app.register_blueprint(search_bp)
+
+if __name__ == "__main__":
     app.run(debug=True)
--- pages/contacts.py
+++ pages/contacts.py
@@ -1,16 +1,11 @@
 # pages/contacts.py
-# Blueprint modelo
 
 from flask import Blueprint, render_template
 
 contacts_bp = Blueprint("contacts", __name__)
 
-@contacts_bp.route("/contacts")
-def contacts_page(): 
-    page_title = "Faça contato"
-    return render_template("contacts.html", page_title=page_title)
 
-@contacts_bp.route("/privacy")
-def privacy_page():
-    page_title = "Políticas de Privacidade"
-    return render_template("privacy.html", page_title=page_title)
+@contacts_bp.route("/contacts")
+def contacts_page():
+    page_title = "Faça Contato"
+    return render_template("contacts.html", page_title=page_title)
